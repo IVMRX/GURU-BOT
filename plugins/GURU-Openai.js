a4fe1e6434e3b0f15b189927a04319a0d5c47a2c
OMAR! <157899772+IVMRX@users.noreply.github.com>
2024-02-07 12:33:08 +0100

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -12,8 +12,8 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
   try {
     m.react(rwait)
     const { key } = await conn.sendMessage(m.chat, {
-      image: { url: 'https://telegra.ph/file/c3f9e4124de1f31c1c6ae.jpg' },
-      caption: 'Thinking....'
+      image: { url: 'https://i.imgur.com/PkJ6TC4.jpg' },
+      caption: '_*سوف يتم تلبية طلبك...*_ \n *تابعنا على انستجرام* \n \n _https://www.instagram.com/ovmar_1_'
     }, {quoted: m})
     conn.sendPresenceUpdate('composing', m.chat);
     const prompt = encodeURIComponent(text);
@@ -27,7 +27,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
 
       if (!result) {
         
-        throw new Error('No valid JSON response from the first API');
+        throw new Error('خطإ في الوصول الى البحث الذي تريده..');
       }
 
       await conn.relayMessage(m.chat, {
@@ -41,7 +41,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
       }, {});
       m.react(done);
     } catch (error) {
-      console.error('Error from the first API:', error);
+      console.error('هناك خطأ:', error);
 
   
       const model = 'llama';
