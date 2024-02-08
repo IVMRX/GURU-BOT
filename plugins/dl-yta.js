4be31154cc7cb389a2f9faa8b9ee47ff1b101ab9
OMAR! <157899772+IVMRX@users.noreply.github.com>
2024-02-08 15:52:08 +0100

    Update dl-yta.js

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -2,7 +2,7 @@ import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
 
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
   if (!args || !args[0]) throw `✳️ Example :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`;
-  if (!args[0].match(/youtu/gi)) throw `❎ Verify that it is a YouTube link.`;
+  if (!args[0].match(/youtu/gi)) throw `*تحقق من الرابط. .*.`;
 
   m.react(rwait); 
 
@@ -23,9 +23,9 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
       { mimetype: 'audio/mpeg' }
     );
 
-    m.react(xmoji); 
+    m.react(done); 
   } catch {
-    await m.reply(`❎ Error: Could not download the audio.`)
+    await m.reply(`*لايمكن. تحميل هاذه الأغنية...`)
   }
 };
 
