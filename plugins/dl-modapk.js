e4b3abb8f742821d695ca98b00923ed905addc27
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-17 11:45:08 +0100

    Update dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
       await conn.reply(m.chat, global.wait, m);
       let data = await download(text);
 
-      if (data.size.replace(' MB', '') > 200) {
+      if (data.size.replace(' MB', '') > 500) {
         return await conn.sendMessage(m.chat, { text: '*هاذا التطبيق كبير جدا. ..*' }, { quoted: m });
       }
 
